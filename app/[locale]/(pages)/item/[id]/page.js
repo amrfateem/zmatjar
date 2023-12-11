7343640f24894bbd2cb1f4c385964fa7d3ad2125
Amr Fateem <development@digializer.com>
2023-12-11 23:29:02 +0200

    Update page.js

--- app/[locale]/(pages)/item/[id]/page.js
+++ app/[locale]/(pages)/item/[id]/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { modalDataState, selectedItemState } from "@/app/atoms";
+import { modalDataState, selectedItemState } from "@/app/[locale]/atoms";
 import { Modal } from "flowbite-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
