19b53954de579f2292a0180b29851e06def360d8
Amr Fateem <development@digializer.com>
2023-12-15 00:13:53 +0200

    fix

--- app/[locale]/(pages)/place-order/components/PlaceOrderBody.js
+++ app/[locale]/(pages)/place-order/components/PlaceOrderBody.js
@@ -78,24 +78,33 @@ function PlaceOrderBody({ time, locale }) {
   const handleTimeChange = (e) => {
     const [minhours, minminutes] = minTime.split(":").map(String);
     const currentDate = new Date();
-    const OrderTime = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}T${e.target.value}`);
-    const minTimeNow = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}T${minhours}:${minminutes}`);
+    const OrderTime = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth()+ 1}-${currentDate.getDate()}T${e.target.value}`);
+    const minTimeNow = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth()+ 1}-${currentDate.getDate()}T${minhours}:${minminutes}`);
 
     const selectedTime = e.target.value;
     const scheduledDateTime = OrderTime
     
 
-    if (differenceInMinutes( OrderTime, minTimeNow ) > 1 && selectedTime < minTime && selectedTime < maxTime ) {
+    if (differenceInMinutes( OrderTime, minTimeNow ) > 1 && selectedTime > minTime ) {
       // If order is in late time during hours
-      setWarning1(true);
+      // setWarning1(true);
       setWarning2(false);
       setWarning3(false);
       console.log("here");
+      setSelectedTime(selectedTime);
+      const year = scheduledDateTime.getUTCFullYear();
+      const month = String(scheduledDateTime.getUTCMonth() + 1).padStart( 2, "0" );
+      const day = String(scheduledDateTime.getUTCDate()).padStart(2, "0");
+      const hours = String(scheduledDateTime.getUTCHours()).padStart(2, "0");
+      const minutes = String(scheduledDateTime.getUTCMinutes()).padStart( 2, "0" );
+      const formattedDateTime = `${year}-${month}-${day}T${ hours + ":" + minutes }:00`;
+      setDeliveryTime(formattedDateTime); 
 
     }
     else if  (selectedTime < minTime && selectedTime < maxTime) {
+      // setWarning2(true);  
       setSelectedTime(selectedTime);
-      setWarning2(true);
+
       setWarning3(true);
       console.log("here");
 
@@ -180,106 +189,106 @@ function PlaceOrderBody({ time, locale }) {
     // setSending(true);
     e.preventDefault();
 
-    console.log(deliveryTime);
+    // console.log(deliveryTime);
 
-    // let headers = new Headers();
-    // headers.append("Content-Type", "application/json");
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
 
-    // let data = JSON.stringify({
-    //   name: e.target.name.value,
-    //   phone: phone,
-    //   countryCode: countryCode.toLocaleUpperCase(),
-    //   address: e.target.address.value,
-    //   email: e.target.email.value,
-    //   paymentMethod: e.target.payment.value,
-    //   language: "en",
-    //   communcationLanguage: storeLanguage,
-    //   scheduledDelivery: deliveryTime,
-    //   order: order,
-    //   subtotal: subtotal.toFixed(2),
-    //   charges: charges,
-    //   total: total,
-    //   coordinates: bypassGeo ? "" : `${location.lat}, ${location.lng}`,
-    //   specialInfo: specialInfo,
-    //   host: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
-    //   telegramChatId: telegramChatId,
-    // });
+    let data = JSON.stringify({
+      name: e.target.name.value,
+      phone: phone,
+      countryCode: countryCode.toLocaleUpperCase(),
+      address: e.target.address.value,
+      email: e.target.email.value,
+      paymentMethod: e.target.payment.value,
+      language: "en",
+      communcationLanguage: storeLanguage,
+      scheduledDelivery: deliveryTime,
+      order: order,
+      subtotal: subtotal.toFixed(2),
+      charges: charges,
+      total: total,
+      coordinates: bypassGeo ? "" : `${location.lat}, ${location.lng}`,
+      specialInfo: specialInfo,
+      host: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+      telegramChatId: telegramChatId,
+    });
 
-    // let requestOptions = {
-    //   method: "POST",
-    //   headers: headers,
-    //   body: data,
-    //   redirect: "follow",
-    // };
+    let requestOptions = {
+      method: "POST",
+      headers: headers,
+      body: data,
+      redirect: "follow",
+    };
 
-    // const polygonCoords = [
-    //   [55.14743, 25.1245014],
-    //   [55.0018611, 25.0025914],
-    //   [55.0046077, 24.8955094],
-    //   [55.1309505, 24.7833473],
-    //   [55.3589168, 24.8132671],
-    //   [55.6473079, 24.8780687],
-    //   [55.8999934, 25.1443936],
-    //   [55.9247127, 25.3232768],
-    //   [55.836822, 25.5935836],
-    //   [55.6198421, 25.6282578],
-    //   [55.4715266, 25.5242049],
-    //   [55.3232112, 25.4225424],
-    //   [55.2133479, 25.2711297],
-    //   [55.1501765, 25.196595],
-    //   [55.14743, 25.1245014],
-    // ];
+    const polygonCoords = [
+      [55.14743, 25.1245014],
+      [55.0018611, 25.0025914],
+      [55.0046077, 24.8955094],
+      [55.1309505, 24.7833473],
+      [55.3589168, 24.8132671],
+      [55.6473079, 24.8780687],
+      [55.8999934, 25.1443936],
+      [55.9247127, 25.3232768],
+      [55.836822, 25.5935836],
+      [55.6198421, 25.6282578],
+      [55.4715266, 25.5242049],
+      [55.3232112, 25.4225424],
+      [55.2133479, 25.2711297],
+      [55.1501765, 25.196595],
+      [55.14743, 25.1245014],
+    ];
 
-    // const currentLocation = [location.lng, location.lat];
-    // const isWithinPolygon = turf.booleanPointInPolygon(
-    //   turf.point(currentLocation),
-    //   turf.polygon([polygonCoords])
-    // );
+    const currentLocation = [location.lng, location.lat];
+    const isWithinPolygon = turf.booleanPointInPolygon(
+      turf.point(currentLocation),
+      turf.polygon([polygonCoords])
+    );
 
-    // if (Object.keys(order).length == 0) {
-    //   setErrorModal(true);
-    //   setModalErrormsg(cartError);
-    //   setSending(false);
-    // } else if (subtotal.toFixed(2) < minimumOrder) {
-    //   setErrorModal(true);
-    //   setModalErrormsg(subTotalError);
-    //   setSending(false);
-    // } else if (!bypassGeo && !isWithinPolygon) {
-    //   setErrorModal(true);
-    //   setModalErrormsg("We are sorry, we don't deliver to your location");
-    //   setSending(false);
-    // } else if (!isValidPhoneNumber(phone).isValid()) {
-    //   setPhoneError(true);
-    //   setSending(false);
-    // } else {
-    //   try {
-    //     const response = await fetch(
-    //       `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/place-order`,
-    //       requestOptions
-    //     );
+    if (Object.keys(order).length == 0) {
+      setErrorModal(true);
+      setModalErrormsg(cartError);
+      setSending(false);
+    } else if (subtotal.toFixed(2) < minimumOrder) {
+      setErrorModal(true);
+      setModalErrormsg(subTotalError);
+      setSending(false);
+    } else if (!bypassGeo && !isWithinPolygon) {
+      setErrorModal(true);
+      setModalErrormsg("We are sorry, we don't deliver to your location");
+      setSending(false);
+    } else if (!isValidPhoneNumber(phone).isValid()) {
+      setPhoneError(true);
+      setSending(false);
+    } else {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/place-order`,
+          requestOptions
+        );
 
-    //     if (!response.ok) {
-    //       throw new Error(`HTTP error! Status: ${response.status}`);
-    //     }
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-    //     const responseData = await response.json();
-    //     console.log("Success:", responseData);
-    //     setOrder([]);
-    //     setSubtotal(0);
-    //     setSum(0);
-    //     setCount(0);
-    //     setSending(false);
-    //     router.push(`/${locale}/thank-you`, undefined, { shallow: true });
-    //   } catch (error) {
-    //     setOrder([]);
-    //     setSubtotal(0);
-    //     setSum(0);
-    //     setCount(0);
-    //     console.error("Error:", error);
-    //     setSending(false);
-    //     router.push(`/${locale}/thank-you`, undefined, { shallow: true });
-    //   }
-    // }
+        const responseData = await response.json();
+        console.log("Success:", responseData);
+        setOrder([]);
+        setSubtotal(0);
+        setSum(0);
+        setCount(0);
+        setSending(false);
+        router.push(`/${locale}/thank-you`, undefined, { shallow: true });
+      } catch (error) {
+        setOrder([]);
+        setSubtotal(0);
+        setSum(0);
+        setCount(0);
+        console.error("Error:", error);
+        setSending(false);
+        router.push(`/${locale}/thank-you`, undefined, { shallow: true });
+      }
+    }
   };
   return (
     <div>
